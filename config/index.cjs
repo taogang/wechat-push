2365dce880b808627d2d2be46fb8acbaf163db0e
陶刚 <taogang649092@163.com>
2023-08-21 16:23:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -11,7 +11,7 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx73fef6e7bb190b62',
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '313befbf08d8ecf2114bbb2af4978896',
+  APP_SECRET: '3646343336d975f2e760c9657cbce1b1',
 
   PROVINCE: '上海',
   CITY: '上海',
@@ -30,22 +30,22 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝宝', year: '1996', date: '02-01',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝宝', year: '1994', date: '01-12',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相恋纪念日', year: '2021', date: '11-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-01-12' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2023-10-05' },
       ],
     },
   ],
